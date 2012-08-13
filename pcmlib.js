655e37bac9fe352f2201fdb5c289d141949c49b6
ignitios <ignitios@gmail.com>
2012-08-13 13:40:26 +0900

    IDの取得を追加した

--- pcmlib.js
+++ pcmlib.js
@@ -2,3 +2,4 @@ var pcmApp = new ActiveXObject("Pcm.App");
 var pcmDoc = pcmApp.GetPcmDoc();
 var pcmPrj = pcmDoc.GetProject();
 var pcmLayer = pcmPrj.SearchLayer("hoge")
+var id = pcmLayer.GetID(3);
