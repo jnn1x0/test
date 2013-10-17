ee7fb58f9174181479840a77c06e0f76f70a9d32
jnn1x0 <jnn1x0@gmail.com>
2013-10-17 17:24:01 +0900

    IDを取得するベクタ要素を変更した。

--- pcmlib.js
+++ pcmlib.js
@@ -2,4 +2,4 @@ var pcmApp = new ActiveXObject("Pcm.App");
 var pcmDoc = pcmApp.GetPcmDoc();
 var pcmPrj = pcmDoc.GetProject();
 var pcmLayer = pcmPrj.SearchLayer("hoge")
-var id = pcmLayer.GetID(3);
+var id = pcmLayer.GetID(4);
