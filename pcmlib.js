e62ab9ba152e0a2c8d970d6bb298deb90abbd37f
jnn1x0 <jnn1x0@gmail.com>
2013-10-17 18:05:36 +0900

    DBタイトルを取得するようにした。

--- pcmlib.js
+++ pcmlib.js
@@ -3,3 +3,5 @@ var pcmDoc = pcmApp.GetPcmDoc();
 var pcmPrj = pcmDoc.GetProject();
 var pcmLayer = pcmPrj.SearchLayer("hoge")
 var id = pcmLayer.GetID(4);
+var pcmLayerDB = pcmLayer.GetDB(4); // DBオブジェクトを取得します。
+var dbTitle = pcmLayerDB.Title;
